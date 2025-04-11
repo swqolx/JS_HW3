427da3f11b7511d55c699149dc0f9e811f6be673
swqolx <153764765+swqolx@users.noreply.github.com>
2025-04-11 19:22:23 +0200

    1

---
+++ hw3.js
@@ -0,0 +1,36 @@
+//execise 1
+let result
+result = 5+5+"5"
+console.log(result);
+console.log(typeof result);
+//=========
+
+
+
+//execise 2
+const email = "tralarelotralala@gmail.com"
+console.log(email.length);
+console.log(email.includes("@"));
+
+//=========
+
+
+
+//execisec 3 
+const first = "My"
+const second = "name"
+const third = "is"
+const fullName = first + " " + second + " " + third + " " + "Viktor"
+console.log(fullName);
+//=========
+
+
+
+//execise 4
+const userName = "Nasta"
+const payment = "300 гривень"
+alert(`Дякуємо, ${userName}! До сплати ${payment}`)
+//=========
+
+
+
